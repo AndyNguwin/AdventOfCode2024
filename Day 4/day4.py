2079592a64c9385bb055dda318f23c69f50fd9f1
Andy Nguyen <iam.andynguwin@gmail.com>
2024-12-11 20:29:37 -0800

    Checked straight lines

--- Day 4/day4.py
+++ Day 4/day4.py
@@ -3,18 +3,14 @@ class Day4_Part1:
         self.filepath = filepath
         self.height = 0
         self.width = 0
-        self.crossword = []
-        self._parse_file_for_crossword(filepath)
+        self.grid = []
+        self._parse_file_for_grid()
     
-    def _parse_file_for_crossword(self, filepath):
+    def _parse_file_for_grid(self):
         with open(self.filepath) as file:
-            self.crossword = file.readlines()
-        self.height = len(self.crossword)
-        self.width = len(self.crossword[0])
-
-    
-    def _check_diagonals(self, row, col):
-        pass
+            self.grid = file.readlines()
+        self.height = len(self.grid)
+        self.width = len(self.grid[0])
 
     def _check_to_top_left(self, row, col):
         pass
@@ -28,25 +24,48 @@ class Day4_Part1:
     def _check_to_bottom_right(self, row, col):
         pass
 
-    def _check_straights(self, row, col):
+    def _check_diagonals(self, row, col):
         pass
 
     def _check_to_left(self, row, col):
-        pass
+        if col < 3: return False
+        elif (self.grid[row][col - 1] == "M" and
+              self.grid[row][col - 2] == "A" and
+              self.grid[row][col - 3] == "S"):
+            return True
+        return False
 
     def _check_to_right(self, row, col):
-        pass
+        if self.width - col < 4: return False
+        elif (self.grid[row][col + 1] == "M" and
+              self.grid[row][col + 2] == "A" and
+              self.grid[row][col + 3] == "S"):
+            return True
+        return False
 
     def _check_to_top(self, row, col):
         if row < 3: return False
-        elif (self.crossword[row - 1][col] == "M" and
-                self.crossword[row - 2][col] == "A" and
-                self.crossword[row - 3][col] == "S"):
+        elif (self.grid[row - 1][col] == "M" and
+                self.grid[row - 2][col] == "A" and
+                self.grid[row - 3][col] == "S"):
                 return True
         return False
 
     def _check_to_bottom(self, row, col):
-        pass
+        if (self.height - row) < 4: return False
+        elif (self.grid[row + 1][col] == "M" and
+              self.grid[row + 2][col] == "A" and
+              self.grid[row + 3][col] == "S"):
+            return True
+        return False
+
+def _check_straights(self, row, col):
+    if (self._check_to_left(row, col) or
+        self._check_to_right(row, col) or
+        self._check_to_top(row, col) or
+        self._check_to_bottom(row, col)):
+        return True
+    return False
 
     def search_for_xmas(self):
         if self.height < 4 and self.width < 4: return 0
