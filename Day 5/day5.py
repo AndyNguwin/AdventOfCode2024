02665f6a23f344fd0c757b5321f071f5acdae9a6
Andy Nguyen <iam.andynguwin@gmail.com>
2024-12-12 22:47:01 -0800

    Completed Day 5 Part 2

--- Day 5/day5.py
+++ Day 5/day5.py
@@ -1,12 +1,15 @@
 from collections import defaultdict
 
 
-class Day5_Part1:
+class Day5:
     def __init__(self, filepath):
         self.filepath = filepath
         self.rules = defaultdict(list)
         self.update_orders = []
+        self.correct_orders = set()
+        self.incorrect_orders = set()
         self._parse_file()
+        self._categorize_update_orders()
 
     def _parse_file(self):
         second_section = False
@@ -20,25 +23,55 @@ class Day5_Part1:
                     values = line.strip().split("|")
                     self.rules[values[0]].append(values[1])
 
-    def get_middle_number_total_from_correct_update_orders(self):
-        total = 0
-        for update_order in self.update_orders:
-            add_total = True
+    def _categorize_update_orders(self):
+        for update_order in enumerate(self.update_orders):
+            correct = True
             discovered = set()
-            for page1 in update_order:
+            for page1 in update_order[1]:
                 if page1 in self.rules.keys():
                     for page2 in self.rules[page1]:
                         if page2 in discovered:
-                            add_total = False
+                            correct = False
                             break
                 discovered.add(page1)
-            if add_total:
-                total += int(update_order[len(update_order) // 2])
+            if correct:
+                self.correct_orders.add(update_order[0])
+            else:
+                self.incorrect_orders.add(update_order[0])
+
+
+    def get_middle_number_total_from_correct_update_orders(self):
+        total = 0
+        for i in self.correct_orders:
+            correct_order = self.update_orders[i]
+            total += int(correct_order[len(correct_order) // 2])
+        return total
+
+    def get_middle_number_total_from_incorrect_update_orders(self):
+        total = 0
+        for i in self.incorrect_orders:
+            incorrect_order = self.update_orders[i]
+            new_order = incorrect_order[:]
+            for page1_i in range(len(new_order)):
+                page1 = incorrect_order[page1_i]
+                page1_new_i = page1_i
+                if page1 in self.rules.keys():
+                    for page2 in self.rules[page1]:
+                        try:
+                            page2_i = new_order.index(page2)
+                            if page2_i < page1_new_i:
+                                new_order.pop(page1_new_i)
+                                new_order.insert(page2_i, page1)
+                                page1_new_i = page2_i
+                        except ValueError:
+                            pass
+            total += int(new_order[len(new_order) // 2])
         return total
 
 
 
 if __name__ == "__main__":
     filepath = "./input.txt"
-    day5_part1 = Day5_Part1(filepath)
-    print(f"Part 1: {day5_part1.get_middle_number_total_from_correct_update_orders()}")
+    day5 = Day5(filepath)
+    print(f"Part 1: {day5.get_middle_number_total_from_correct_update_orders()}")
+    print(f"Part 2: {day5.get_middle_number_total_from_incorrect_update_orders()}")
