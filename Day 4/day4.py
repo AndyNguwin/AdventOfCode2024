93745ed2765f9a61756f42a692705b6aab225229
Andy Nguyen <iam.andynguwin@gmail.com>
2024-12-12 20:28:00 -0800

    Day 4 Part 2 completed

--- Day 4/day4.py
+++ Day 4/day4.py
@@ -94,9 +94,54 @@ class Day4_Part1:
 
     def get_xmas_count(self):
         return self.xmas_count
-    
+
+
+class Day4_Part2:
+    def __init__(self, filepath):
+        self.filepath = filepath
+        self.height = 0
+        self.width = 0
+        self.grid = []
+        self.xmas_count = 0
+        self._parse_file_for_grid()
+        self._search_for_xmas()
+
+    def _parse_file_for_grid(self):
+        with open(self.filepath) as file:
+            self.grid = file.readlines()
+        self.height = len(self.grid)
+        self.width = len(self.grid[0])
+
+    def _check_top_left_bottom_right(self, row, col):
+        if ((self.grid[row - 1][col - 1] == "M" and self.grid[row + 1][col + 1] == "S") or
+            (self.grid[row - 1][col - 1] == "S" and self.grid[row + 1][col + 1] == "M")):
+            return True
+        return False
+
+    def _check_top_right_bottom_left(self, row, col):
+        if ((self.grid[row - 1][col + 1] == "M" and self.grid[row + 1][col - 1] == "S") or
+            (self.grid[row - 1][col + 1] == "S" and self.grid[row + 1][col - 1] == "M")):
+            return True
+        return False
+
+    def _check_corners(self, row, col):
+        if row == 0 or row == self.height - 1 or col == 0 or col == self.width - 1: return
+        if (self._check_top_left_bottom_right(row, col) and self._check_top_right_bottom_left(row, col)):
+            self.xmas_count += 1
+
+    def _search_for_xmas(self):
+        for row in range(self.height):
+            for col in range(self.width):
+                if self.grid[row][col] == "A":
+                    self._check_corners(row, col)
+
+    def get_xmas_count(self):
+        return self.xmas_count
+
 
 if __name__ == "__main__":
     filepath = "./input.txt"
     day4_part1 = Day4_Part1(filepath)
-    print(f"Day 1: {day4_part1.get_xmas_count()} instances of 'XMAS'")
+    day4_part2 = Day4_Part2(filepath)
+    print(f"Part 1: {day4_part1.get_xmas_count()} instances of 'XMAS'")
+    print(f"Part 2: {day4_part2.get_xmas_count()} instances of 'XMAS'")
