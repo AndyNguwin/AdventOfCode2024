987e24a718eddc64416ee3158c1430f1b26c6ace
= <iam.andynguwin@gmail.com>
2024-12-15 01:07:56 -0800

    Day 7 Part 2 Completed

--- Day 7/day7.py
+++ Day 7/day7.py
@@ -1,24 +1,37 @@
-class Day7_Part1:
+class Day7:
     def __init__(self, filepath):
         self.filepath = filepath
         self.calibration_total = 0
-        self._calculate_calibration()
+        # self._calculate_calibration()
     
     def _check_for_add_or_mul_to_be_true(self, values, current, target):
         if len(values) == 0:
             return True if current == target else False
+        # if current >= target: return False
         current_val = values[0]
         add_to_current = current + current_val
         mul_to_current = current * current_val
-        # print("Target: {target}")
-        # print(f"Add {current_val} to {current}: {add_to_current}")
-        # print(f"Muk {current_val} to {current}: {mul_to_current}")
+
         if (self._check_for_add_or_mul_to_be_true(values[1:], add_to_current, target) or
             self._check_for_add_or_mul_to_be_true(values[1:], mul_to_current, target)):
             return True
         return False
     
-    def _calculate_calibration(self):
+    def _check_for_add_mul_concat_to_be_true(self, values, current, target):
+        if len(values) == 0:
+            return True if current == target else False
+        # if current >= target: return False
+        current_val = values[0]
+        add_to_current = current + current_val
+        mul_to_current = current * current_val
+        concat_to_current = int(str(current) + str(current_val))
+        if (self._check_for_add_mul_concat_to_be_true(values[1:], add_to_current, target) or
+            self._check_for_add_mul_concat_to_be_true(values[1:], mul_to_current, target) or
+            self._check_for_add_mul_concat_to_be_true(values[1:], concat_to_current, target)):
+            return True
+        return False
+    
+    def _calculate_calibration(self, part):
         with open(self.filepath) as file:
             self.calibration_total = 0
             for equation in file:
@@ -26,13 +39,20 @@ class Day7_Part1:
                 equation_split = equation.split(": ")
                 target = int(equation_split[0])
                 values = [int(value) for value in equation_split[1].split()]
-                if (self._check_for_add_or_mul_to_be_true(values, 0, target)):
-                    self.calibration_total += target
+                if part == 1:
+                    if (self._check_for_add_or_mul_to_be_true(values[1:], values[0], target)):
+                        self.calibration_total += target
+                elif part == 2:
+                    if (self._check_for_add_mul_concat_to_be_true(values[1:], values[0], target)):
+                        self.calibration_total += target
     
-    def get_calibration_total(self):
+    def get_calibration_total(self, part):
+        self._calculate_calibration(part)
         return self.calibration_total
+    
 
 if __name__ == "__main__":
     filepath = "./input.txt"
-    day7_part1 = Day7_Part1(filepath)
-    print(f"Part 1: {day7_part1.get_calibration_total()}")
+    day7 = Day7(filepath)
+    print(f"Part 1: {day7.get_calibration_total(1)}")
+    print(f"Part 2: {day7.get_calibration_total(2)}")
